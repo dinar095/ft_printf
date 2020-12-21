8d44e67f863e3f5051bae3fab370e0c6407e61e9
Desausag <dinar095@hotmail.com>
2020-12-21 19:29:48 +0300

    add va_arg in flags

--- main.c
+++ main.c
@@ -30,23 +30,36 @@ void	flags(char **str, p_list *list)//флаги
 		}
 }
 //wide function
-void 	wide(char **str, p_list *list)
+void 	wide(char **str, p_list *list, va_list ap)
 {
 	if (**str > '0' && **str <= '9')
+	{
 		list->wide = ft_atoi(*str);
-	while (**str > '0' && **str <= '9')
+		while (**str > '0' && **str <= '9')
+			(*str)++;
+	}
+	else if (**str == '*')
+	{
+		list->wide = va_arg(ap, int);
 		(*str)++;
+	}
 }
-void 	precision(char **str, p_list *list)
+void 	precision(char **str, p_list *list, va_list ap)
 {
 	if (**str == '.')
 	{
 		list->precision = 0;
 		(*str)++;
 		if (**str > '0' && **str <= '9')
+		{
 			list->precision = ft_atoi(*str);
-		while (**str > '0' && **str <= '9')
-			(*str)++;
+			while (**str > '0' && **str <= '9')
+				(*str)++;
+		}
+		else if (**str == '*')
+		{
+			list->precision = va_arg(ap, int);
+		}
 	}
 }
 // Precision function
@@ -69,8 +82,8 @@ void	parser(char **str, va_list ap)
 		while (**str != '\0' && **str != '%' && **str !='d')                                //возможно надо удалить
 		{
 			flags(str, list);
-			wide(str, list);
-			precision(str, list);
+			wide(str, list, ap);
+			precision(str, list, ap);
 
 
 		}
@@ -102,9 +115,9 @@ int main(void)
 {
 char *s = "Heloooo";
 //char *p = &s[3];
-ft_printf("text before %-1.45dyhy\n", "Hello", "/bls|");
+ft_printf("text before %-*.45dyhy\n", 20, "Hello", "/bls|");
 //printf("text before %%%strtr", "Hello");
 //ft_putstr(&s, (p - s));
-printf("\n%0.5d\n", 589);
+//printf("\n%*60.50d\n", 100, 589);
 	return 0;
 }
